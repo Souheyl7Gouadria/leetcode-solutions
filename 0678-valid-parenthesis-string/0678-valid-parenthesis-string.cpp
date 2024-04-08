d5cd29f0bdad03e9370347153a3f42c8f95513fc
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-04-08 15:03:10 +0100

    Time: 3 ms (45.67%), Space: 7.5 MB (27.41%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int opencnt = 0;
-        int closecnt = 0;
+        stack <int> open;
+        stack <int> stars;
         int n = s.length();
         for(int i=0;i<n;i++){
-            if(s[i] =='(' || s[i] =='*'){
-                opencnt++;
+            if(s[i] == '('){
+                open.push(i);
+            }else if(s[i] == '*'){
+                stars.push(i);
             }else{
-                opencnt--;
+                if(!open.empty()){
+                    open.pop();
+                }else if(!stars.empty()){
+                    stars.pop();
+                }else{
+                    return false;
+                }
             }
-            if(s[n-1-i] == ')' || s[n-1-i] == '*'){
-                closecnt++;
+        }
+        while(!open.empty()){
+            if(!stars.empty()){
+                if(open.top()>stars.top()){
+                    return false;
+                }else{
+                    open.pop();
+                    stars.pop();
+                }
             }else{
-                closecnt--;
-            }
-            if(opencnt < 0 || closecnt < 0){
                 return false;
             }
         }
-        
         return true;
     }
 };
