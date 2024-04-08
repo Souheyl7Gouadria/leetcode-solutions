b3762c582bcfcf86a2d79358cb52b2e2d44e1fab
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-04-08 15:05:59 +0100

    Update 0678-valid-parenthesis-string.cpp

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,3 +1,5 @@
+// two stacks approach !!!
+
 class Solution {
 public:
     bool checkValidString(string s) {
@@ -34,3 +36,32 @@ public:
         return true;
     }
 };
+
+
+// two pointers approach !!!
+
+// class Solution {
+// public:
+//     bool checkValidString(string s) {
+//         int opencnt = 0;
+//         int closecnt = 0;
+//         int n = s.length();
+//         for(int i=0;i<n;i++){
+//             if(s[i] =='(' || s[i] =='*'){
+//                 opencnt++;
+//             }else{
+//                 opencnt--;
+//             }
+//             if(s[n-1-i] == ')' || s[n-1-i] == '*'){
+//                 closecnt++;
+//             }else{
+//                 closecnt--;
+//             }
+//             if(opencnt < 0 || closecnt < 0){
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+// };
