7c12176fbc628095d85f7d2285383fa063d2bfdc
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-05-04 16:27:20 +0100

    Time: 5 ms (51.55%), Space: 16.4 MB (37.15%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -4,14 +4,22 @@ public:
     int mincost(int pos,vector<int>& cost,vector<int>& dp){
         int n = cost.size();
         // base case 
-        if(pos >= n) return 0;
         if(dp[pos] != -1) return dp[pos];
         return dp[pos] = cost[pos] + min(mincost(pos+1,cost,dp),mincost(pos+2,cost,dp));
     }
     
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector <int> dp(n,-1);
-        return min(mincost(0,cost,dp),mincost(1,cost,dp));
+        vector <int> dp(n+1,0);
+        dp[n] = 0;
+        for(int i=n-1;i>=0;i--){
+            int oneStep=dp[i+1];
+            int twoSteps=1000;
+            if(i<=n-2){
+                twoSteps=dp[i+2];
+            }
+            dp[i] = cost[i] + min(oneStep,twoSteps);
+        }
+        return min(dp[0],dp[1]);
     }
 };
