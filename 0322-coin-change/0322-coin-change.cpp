a29610932d02f1d0f20ae7aad5240338b5b24a7d
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-05-04 22:39:09 +0100

    Time: 55 ms (17.81%), Space: 51.1 MB (12.83%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     
-    int minNumber(int idx, int amount, vector<int>& coins,vector <vector<int>> &dp){
-        // base case
-        if(idx == 0){
-            if(amount%coins[0] == 0){
-                return amount/coins[0];
-            }else{
-                return 1e9;
-            }
-        }
-        if(dp[idx][amount] != -1) return dp[idx][amount];
-        int notTake = minNumber(idx-1,amount,coins,dp);
-        int take = 1e9;
-        if(amount >= coins[idx]){
-            take = 1 + minNumber(idx,amount-coins[idx],coins,dp);
-        }
-        return dp[idx][amount] = min(take,notTake);
-        
-    }
-    
     int coinChange(vector<int>& coins, int amount) {
         sort(coins.begin(),coins.end());
         int n = coins.size();
-        vector <vector<int>> dp(n,vector <int> (amount+1,-1));
-        int response =  minNumber(n-1,amount,coins,dp);
+        vector <vector<int>> dp(n+1,vector <int> (amount+1,0));
+        // base case
+        for(int t = 0;t<=amount;t++){
+            if(t%coins[0] == 0){
+                dp[0][t] = t/coins[0];
+            }else{
+                dp[0][t] = 1e9;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=amount;t++){
+                int notTake = dp[i-1][t];
+                int take = 1e9;
+                if(coins[i]<=t){
+                    take = 1+dp[i][t-coins[i]];
+                }
+                dp[i][t] = min(take,notTake);
+            }
+        }
+        
+        int response = dp[n - 1][amount];
         if(response == 1e9){
             return -1;
         }
