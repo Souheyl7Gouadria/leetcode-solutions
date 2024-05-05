af00edcebc9b0a2584fe38ccc55d30ef01912056
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-05-05 17:54:07 +0100

    Time: 32 ms (6.77%), Space: 16.1 MB (5.00%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    string minRemoveToMakeValid(std::string s) {
+    string minRemoveToMakeValid(string s) {
+        int open = 0;
+        int close = 0;
+        stack <int> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i] !='(' && s[i] != ')'){
+                st.push(s[i]);
+            }else if(s[i] == '('){
+                open++;
+                st.push(s[i]);
+            }else if(open>0){
+                open--;
+                st.push(s[i]);
+            }
+        }
         string resp = "";
-        string temp = "";
-        int cnt = 0;
-        int n = s.length();
-        
-        for(int i = 0; i < n; i++) {
-            if(s[i] != ')' && s[i] != '(') {
-                temp += s[i];
-            } else if(s[i] == ')') {
-                if(cnt > 0) {
-                    cnt--;
-                    temp += s[i];
-                }
-            } else {
-                cnt++;
-                temp += s[i];
+        while(!st.empty()){
+            char c = st.top();
+            st.pop();
+            if(c != '(' && c != ')'){
+                resp+=c;
+            }else if(c == ')'){
+                close++;
+                resp+=c;
+            }else if(close > 0){
+                resp+=c;
+                close--;
             }
         }
-        for(int i = temp.size() - 1; i >= 0; i--) {
-            if(temp[i] == '(' && cnt > 0) {
-                cnt--;
-            } else {
-                resp += temp[i];
-            }
-        }
-        reverse(resp.begin(), resp.end());    
+        reverse(resp.begin(),resp.end());
         return resp;
+        
     }
 };
