e38b4905654d7340b806a21f68e3fb5983743ca7
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-05-19 14:34:42 +0100

    Time: 0 ms (100.00%), Space: 16.3 MB (60.73%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
 
-    int helper(vector <int> &cost , vector <int> &dp, int idx){
-        int n = cost.size();
-        // base case 
-        if(idx >= n) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        return dp[idx] = cost[idx] + min(helper(cost ,dp, idx+1),helper(cost ,dp, idx+2));
-    }
-
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector <int> dp(n+1,-1);
-        return min(helper(cost,dp,0),helper(cost,dp,1));
+        vector <int> dp(n+2,0);
+        // base case already set to 0
+        
+        // recursion from 0 -> n , tab will be from n->0
+        for(int i=n-1;i>=0;i--){
+            int moveOne = dp[i+1];
+            int moveTwo = dp[i+2];
+            dp[i] = cost[i] + min(moveOne,moveTwo);
+        }
+        return min(dp[0],dp[1]);
+        
     }
 };
