c0118ebc5defbf03272c6437deed905af989af05
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-10-15 12:09:00 +0100

    Time: 1 ms (84.6%), Space: 41.6 MB (62.15%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,20 +1,20 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         int i = 0;
-        String resp = "";
+        StringBuilder resp = new StringBuilder();
         while(i<word1.length() && i<word2.length()){
-            resp+=word1.charAt(i);
-            resp+=word2.charAt(i);
+            resp.append(word1.charAt(i));
+            resp.append(word2.charAt(i));
             i++;
         }
         while(i<word1.length()){
-            resp+=word1.charAt(i);
+            resp.append(word1.charAt(i));
             i++;
         }
         while(i<word2.length()){
-            resp+=word2.charAt(i);
+            resp.append(word2.charAt(i));
             i++;
         }
-        return resp;
+        return resp.toString();
     }
 }
