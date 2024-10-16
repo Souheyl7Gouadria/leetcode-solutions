c919e655f0335f6abc0d8840196170629b3ba235
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-10-16 12:17:07 +0100

    Time: 26 ms (66.06%), Space: 40.2 MB (33.12%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector <int> prefProd(n);
-        vector <int> suffProd(n);
-        prefProd[0] = nums[0];
-        suffProd[n-1] = nums[n-1];
+        vector <int> front(n),back(n);
+        front[0] = nums[0];
+        back[n-1] = nums[n-1];
         for(int i=1;i<n;i++){
-            prefProd[i]=prefProd[i-1]*nums[i];
+            front[i] = front[i-1]*nums[i];
         }
         for(int i=n-2;i>=0;i--){
-            suffProd[i]=suffProd[i+1]*nums[i];
+            back[i] = back[i+1]*nums[i];
         }
-        vector <int> resp(n);
-        resp[0]=suffProd[1];
-        resp[n-1]=prefProd[n-2];
+        nums[0] = back[1];
+        nums[n-1] = front[n-2];
         for(int i=1;i<n-1;i++){
-            resp[i] = prefProd[i-1]*suffProd[i+1];
+            nums[i] = back[i+1]*front[i-1];
         }
-        return resp;
+        return nums;
     }
 };
