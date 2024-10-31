923035f434573401bb369a6a6d968e4cd823ae82
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-10-31 23:41:53 +0100

    Time: 97 ms (21.12%), Space: 53.2 MB (11.31%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        
         int n = nums.size();
-        vector <int> dp(n,-1);
+        vector<int> dp(n, 0);
         // base case 
         dp[n-1] = 1;
-        // tabulation (bottom up approach)
         for(int i=n-2;i>=0;i--){
-            // 0 is a blocking value, impossible to reach the end
-            if(nums[i] == 0){
-                dp[i] = 0;
-                continue;
-            }
-            // max index to jump to from current position
-            int reach = nums[i]+i;
-            for(int j=i+1;j<=reach;j++){
-                if(j<n && dp[j]){
-                    // can reach the last one from the (i)th index directly or by passing through previous one
+            for(int j=i+1;j<=i+nums[i];j++){
+               if(j<n && dp[j]==1) {
                     dp[i] = 1;
-                    break;
-                }
+                    break; //we can reach the last index in nums from that index
+               }
             }
-            // if the dp[i] is not set to 1 ,then we cant reach the last index
-            if(dp[i] == -1){
-                dp[i] = 0;
-            }
-        } 
+        }
         return dp[0];
     }
 };
