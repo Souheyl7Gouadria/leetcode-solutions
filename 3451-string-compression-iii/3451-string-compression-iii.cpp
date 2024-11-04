adaa7481a751172f18fc2371341289327c63ab3d
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-11-04 14:08:28 +0100

    Time: 13 ms (87.18%), Space: 27.8 MB (83.84%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -6,14 +6,14 @@ public:
         int n = word.size();
         for(int i=1;i<n;i++){
             if(word[i]!=word[i-1] || cnt == 9){
-                comp += to_string(cnt);
+                comp += cnt+'0';
                 comp+=word[i-1];
                 cnt = 1;
             }else{
                 cnt++;
             }
         }
-        comp+=to_string(cnt);
+        comp+=cnt+'0';
         comp+=word[n-1];
         return comp;
     }
