5f6a69781c2bb853b45fbcdf42dfa1149832d7a1
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-11-11 22:02:21 +0100

    Time: 0 ms (100%), Space: 12 MB (24.39%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        stack <string> st;
+        stack<string> st;
         for(int i=0;i<path.size();i++){
-            if(path[i] == '/'){
-                continue;
-            }
+            if(path[i] == '/') continue;
             string temp = "";
             while(i<path.size() && path[i] != '/'){
-                temp+=path[i];
-                i++;
+                temp+=path[i++];
             }
-            if(temp == "."){
-                continue;
-            }else if(temp == ".."){
-                if(!st.empty()){
-                    st.pop();
-                }
-                // if we get ".." and the stack is empty , nthing to do
+            if(temp == ".") continue;
+            if(temp == ".."){
+                if(!st.empty()) st.pop();
             }else{
                 st.push(temp);
             }
-        }
-        string route = "";
+        }        
+        if(st.empty()) return "/";
+        string resp = "";
         while(!st.empty()){
-            route=st.top()+'/'+route;
+            resp='/'+st.top()+resp;
             st.pop();
         }
-        string resp = route.substr(0,route.size()-1);
-        resp = "/"+resp;
         return resp;
     }
 };
