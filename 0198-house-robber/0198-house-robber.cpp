a9789662f2358923a86fa736f329ef2cf292a47a
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-11-16 20:33:44 +0100

    Time: 0 ms (100%), Space: 9.6 MB (12.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    
-    int steal(vector <int> & nums,vector <int> & dp, int index){
-        int n = nums.size();
-        // base case
-        if(index >= n) return 0;
-        if(dp[index] != -1) return dp[index];
-        return dp[index] = nums[index] + max(steal(nums,dp,index+2),steal(nums,dp,index+3));
-        
-    }
-    
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector <int> dp(n+1,-1);
-        return max(steal(nums,dp,0),steal(nums,dp,1));
+        vector <int> dp(n+3,0); 
+        // recursion if from 0 -> n
+        // tabulation will be from n -> 0
+        for(int i=n-1;i>=0;i--){
+            int twoSteps = dp[i+2];
+            int threeSteps = dp[i+3];
+            dp[i] = nums[i] + max(twoSteps,threeSteps);
+        }
+        return max(dp[0],dp[1]);
+        
     }
 };
