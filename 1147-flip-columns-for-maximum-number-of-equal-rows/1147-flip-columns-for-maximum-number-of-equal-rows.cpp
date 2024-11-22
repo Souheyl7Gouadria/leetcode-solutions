9d3342a0a59f5f2c9057ea71a91fee2b80f108f8
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2024-11-23 00:04:19 +0100

    Time: 89 ms (28.17%), Space: 75.6 MB (58.05%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
-        map<string, int> patternCount; 
+        unordered_map<string, int> patternCount; 
         int maxRows = 0;
-
         for (const auto& row : matrix) {
             string pattern = "";        
-            string invertedPattern = ""; 
+            bool startWith0 = row[0] == 0;
             
             for (int val : row) {
-                pattern += to_string(val);
-                invertedPattern += to_string(1 - val);
+                if(startWith0) {
+                     pattern+= to_string(val);
+                }else{
+                     pattern+= to_string(1 - val);
+                } 
             }
             
             patternCount[pattern]++;
             maxRows = max(maxRows, patternCount[pattern]);
-
-            patternCount[invertedPattern]++;
-            maxRows = max(maxRows, patternCount[invertedPattern]);
         }
-
         return maxRows;
     }
 };
