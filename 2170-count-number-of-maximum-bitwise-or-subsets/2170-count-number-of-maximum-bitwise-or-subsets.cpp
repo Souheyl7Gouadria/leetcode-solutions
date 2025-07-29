2847b8811d0f471316bfabc43b9f1bb56ff0f158
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2025-07-29 15:44:00 +0100

    Time: 1682 ms (5.07%), Space: 487.2 MB (5.67%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //backtracking solution in O(n*2^n)
+    //generating subsets with backtracking
     void generateSubsets(int index, vector<int>& nums, vector<int>& subset, vector<vector<int>>& allSubsets){
         if(index == nums.size()){
             allSubsets.push_back(subset);
@@ -14,8 +14,8 @@ public:
         subset.pop_back();
         generateSubsets(index+1,nums,subset,allSubsets);
     }
-
-    int countMaxOrSubsets(vector<int>& nums) {
+    //backtracking solution in O(n*2^n)
+    int SubsetswithBacktracking(vector<int>& nums) {
         vector<vector<int>> allSubsets;
         vector<int> subset;
         generateSubsets(0,nums,subset,allSubsets);
@@ -30,6 +30,35 @@ public:
                 subsetOr|=allSubsets[i][j];
             }
             if(subsetOr == numsOr) result++;
+            subsetOr=0;
+        }
+        return result;
+    }
+    //generating subsets with bitmasks
+    int countMaxOrSubsets(vector<int>& nums) {
+        int n = nums.size();
+        int numberOfSubsets = 1<<n;
+        vector<vector<int>> allSubsets;
+        for(int i=0;i<numberOfSubsets;i++){
+            vector<int> subset;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j)){
+                    subset.push_back(nums[j]);
+                }
+            }
+            allSubsets.push_back(subset);
+        }
+        int numsOr=0;
+        int subsetOr=0;
+        int result=0;
+        for(int i : nums){
+            numsOr|=i;
+        }
+        for(int i=0;i<allSubsets.size();i++){
+            for(int j=0;j<allSubsets[i].size();j++){
+                subsetOr |= allSubsets[i][j];
+            }
+            if (subsetOr == numsOr) result++;
             subsetOr=0;
         }
         return result;
