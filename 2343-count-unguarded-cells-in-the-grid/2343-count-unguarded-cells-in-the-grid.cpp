8efc1a3edd62e2ad625f9c520f0e0f1660c1f915
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2025-11-02 12:39:21 +0100

    Time: 213 ms (22.02%), Space: 209.3 MB (19.73%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        vector<vector<int>> matrix(m,vector<int> (n,0)); // guard 1 , wall 2 , guarded cell 3 , not guarded 0
-        for(int i=0;i<guards.size();i++){
-            matrix[guards[i][0]][guards[i][1]] = 1;
-        }
-        for(int i=0;i<walls.size();i++){
-            matrix[walls[i][0]][walls[i][1]] = 2;
-        }
-
-        auto mark = [&](int r, int c) {
-            // vertically
-            for(int row = r-1; row >= 0 ; row--){
-                if(matrix[row][c] == 1 || matrix[row][c] == 2) break;
-                matrix[row][c] = 3;
-            }
-            for(int row = r+1; row < m ; row++){
-                if(matrix[row][c] == 1 || matrix[row][c] == 2) break;
-                matrix[row][c] = 3;
-            }
-            // vertically
-            for(int col = c-1; col >= 0 ; col--){
-                if(matrix[r][col] == 1 || matrix[r][col] == 2) break;
-                matrix[r][col] = 3;
-            }
-            for(int col = c+1; col < n ; col++){
-                if(matrix[r][col] == 1 || matrix[r][col] == 2) break;
-                matrix[r][col] = 3;
-            }
-        };
-
-        for(int i=0;i<guards.size();i++){
-            mark(guards[i][0],guards[i][1]);
-        }
-        int cnt = 0;
+        vector<vector<int>> matrix(m, vector<int>(n, 0));
+        // 0:free, 1:guard, 2:wall, 3:guardable
+        for(auto v : guards) matrix[v[0]][v[1]] = 1;
+        for(auto v : walls) matrix[v[0]][v[1]] = 2;
+        
+        for(auto v : guards) markGuarded(matrix,v[0],v[1]);
+        int resp = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j] == 0) cnt++;
+                if(!matrix[i][j]) resp++;
             }
         }
-        return cnt;
+        return resp;
     }
-    
+    void markGuarded(vector<vector<int>>& matrix,int r, int c){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        // up
+        for(int row = r-1; row >=0; row--){
+            if(matrix[row][c] == 1 || matrix[row][c] == 2) break;
+            matrix[row][c] = 3;
+        }
+        // down
+        for(int row = r+1 ;row < m; row++){
+            if(matrix[row][c] == 1 || matrix[row][c] == 2) break;
+            matrix[row][c] = 3;
+        }
+        // left
+    for(int col = c-1; col >= 0; col--){
+            if(matrix[r][col] == 1 || matrix[r][col] == 2) break;
+            matrix[r][col] = 3;
+        }
+        // right
+        for(int col = c+1; col < n; col++){
+            if(matrix[r][col] == 1 || matrix[r][col] == 2) break;
+            matrix[r][col] = 3;
+        }
+    }
 };
