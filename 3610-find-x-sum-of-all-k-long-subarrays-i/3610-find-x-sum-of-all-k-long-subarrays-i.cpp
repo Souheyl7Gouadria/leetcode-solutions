fea1118a75d966bc8febd56607c079884ef51876
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2025-11-05 14:23:49 +0100

    Time: 27 ms (18.33%), Space: 37.4 MB (14.79%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,32 +1,31 @@
+bool compareDesc(const pair<int,int>& a, const pair<int,int>& b) {
+    if (a.second == b.second)
+        return a.first > b.first;
+    return a.second > b.second;
+}
+
 class Solution {
 public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
         int n = nums.size();
-        vector<int> ans;
+        vector<int> result;
 
         for (int i = 0; i <= n - k; i++) {
-            vector<int> sub(nums.begin() + i, nums.begin() + i + k);
+            vector<int> subArray(nums.begin() + i, nums.begin() + i + k);
             unordered_map<int, int> freq;
-            for (int num : sub) {
-                freq[num]++;
-            }
-            vector<pair<int, int>> items(freq.begin(), freq.end());
-            sort(items.begin(), items.end(), [](auto &a, auto &b) {
-                if (a.second == b.second) return a.first > b.first;
-                return a.second > b.second;
-            });
-            unordered_set<int> top_x;
-            for (int j = 0; j < items.size() && j < x; j++) {
-                top_x.insert(items[j].first);
-            }
-            int x_sum = 0;
-            for (int num : sub) {
-                if (top_x.count(num)) x_sum += num;
-            }
 
-            ans.push_back(x_sum);
+            for (int num : subArray)
+                freq[num]++;
+
+            vector<pair<int, int>> arr(freq.begin(), freq.end());
+            sort(arr.begin(), arr.end(), compareDesc);
+            int sum = 0;
+            for (int j = 0; j < min(x, (int)arr.size()); j++)
+                sum += arr[j].first * arr[j].second;
+
+            result.push_back(sum);
         }
 
-        return ans;
-    }
+    return result;
+    };
 };
