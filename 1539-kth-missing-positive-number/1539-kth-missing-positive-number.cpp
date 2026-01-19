5966a6e8e6f9d80f52796953c08d132cc40a5ea8
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2026-01-19 23:57:53 +0100

    Time: 0 ms (100%), Space: 13.3 MB (27.28%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
+    // brute forve O(n)
+
+    // int findKthPositive(vector<int>& arr, int k) {
+    //     int cur = k;
+    //     int i = 0;
+    //     while(i < arr.size() && cur >= arr[i]){
+    //         cur++;
+    //         i++;
+    //     }
+    //     return cur;
+    // }
+
+    // Binary search (Striver)
     int findKthPositive(vector<int>& arr, int k) {
-        int cur = k;
-        int i = 0;
-        while(i < arr.size() && cur >= arr[i]){
-            cur++;
-            i++;
+        int l = 0;
+        int r = arr.size()-1;
+        int m;
+        int missing;
+        while(l <= r){
+            m = l+(r-l)/2;
+            missing = arr[m] - (m+1);
+            if(missing < k) l = m+1;
+            else r = m-1;
         }
-        return cur;
+        return k+r+1; // or k+l becaus in the end arr : ..rl.. so l = r+1;
     }
 };
