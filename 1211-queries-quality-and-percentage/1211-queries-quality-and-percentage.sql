9a348722e963477193138241ef5716df617548eb
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2026-03-07 14:42:15 +0100

    Time: 349 ms (62.32%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,4 +1,4 @@
 select query_name , round(avg(rating/position),2) as quality , 
-round(sum(if(rating<3,1,0))*100/(count(*)),2) as poor_query_percentage 
+round(avg(if(rating<3,1,0)*100),2) as poor_query_percentage 
 from Queries 
 group by query_name;
