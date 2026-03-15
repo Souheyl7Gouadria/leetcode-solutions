ebe4a4ae9d1093d440e3ced96c5f3a54e5db317a
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2026-03-15 13:21:50 +0100

    Time: 456 ms (66.6%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,7 @@
+select product_id, 10 as price from Products group by product_id having min(change_date) > '2019-08-16'
+union all # we wont have any duplicate , so use union all instead of union for perf considerations
+select product_id, new_price as price from Products where (product_id, change_date) in (
+    select product_id, max(change_date)
+    from Products where change_date <= '2019-08-16'
+    group by product_id
+)
