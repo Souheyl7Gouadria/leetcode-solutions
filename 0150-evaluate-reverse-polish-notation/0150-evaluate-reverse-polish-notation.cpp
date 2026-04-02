417236a63eada74a325242d2d75cb3c026983a15
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2026-04-02 18:29:20 +0100

    Time: 0 ms (100%), Space: 17.1 MB (41.95%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,52 +1,27 @@
-// THIS solution will give TLE
-
-// class Solution {
-// public:
-//     int evalRPN(vector<string>& tokens) {
-//         while(tokens.size() > 1){
-//             for(int i=0;i<tokens.size();i++){
-//                 if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/"){
-//                     int a = stoi(tokens[i-2]);
-//                     int b = stoi(tokens[i-1]);
-//                     int calc = 0;
-//                     if(tokens[i] == "+") calc = a+b;
-//                     if(tokens[i] == "-") calc = a-b;
-//                     if(tokens[i] == "*") calc = a*b;
-//                     if(tokens[i] == "/") calc = a/b;
-
-//                     tokens.erase(tokens.begin()+i-2,tokens.begin()+i+1);
-//                     tokens.insert(tokens.begin()+i-2,to_string(calc));
-//                     break;
-//                 }
-//             }
-//         }
-//         return stoi(tokens[0]);
-//     }
-// };
-
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
-        for(const string& c : tokens){
-            if( c == "+"){
-                int a = st.top(); st.pop();
-                int b = st.top(); st.pop();
-                st.push(b+a);
-            } else if( c == "-"){
-                int a = st.top(); st.pop();
-                int b = st.top(); st.pop();
-                st.push(b-a);
-            } else if( c == "*"){
-                int a = st.top(); st.pop();
-                int b = st.top(); st.pop();
-                st.push(b*a);
-            } else if( c == "/"){
-                int a = st.top(); st.pop();
-                int b = st.top(); st.pop();
-                st.push(b/a);
+        for(string ele : tokens){
+            if(ele == "+" || ele == "-" || ele == "*" || ele == "/"){
+
+                int elementOne = st.top(); st.pop();
+                int elementTwo = st.top(); st.pop();
+
+                if(ele =="+"){
+                    st.push(elementTwo+elementOne);
+                }
+                else if(ele =="-"){
+                    st.push(elementTwo-elementOne);
+                }
+                else if(ele =="*"){
+                    st.push(elementTwo*elementOne);
+                }
+                else if(ele =="/"){
+                    st.push(elementTwo/elementOne);
+                }
             }else{
-                st.push(stoi(c));
+                st.push(stoi(ele));
             }
         }
         return st.top();
