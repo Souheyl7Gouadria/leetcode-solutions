54dee88a87dbeb3211c9b89bd155127db329bcdc
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2026-04-11 22:02:14 +0100

    Time: 0 ms (100%), Space: 8.4 MB (85.68%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long i = 0;
-        while(i*i<x){
-            i+=1;
+        int l = 0, r = x;
+        long long mid;
+        while(l <= r){
+            mid = l+(r-l)/2;
+            if(mid*mid <= x) l = (int)mid+1;
+            else r = (int)mid-1;
         }
-        if(i*i<=x) return i;
-        return i-1;
+        return l-1;
     }
 };
