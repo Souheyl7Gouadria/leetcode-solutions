1b0bd89b98019584cbf0629dad84c118236ce0a4
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2026-04-11 14:41:06 +0100

    Time: 0 ms (100%), Space: 14.6 MB (15.41%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> st(nums1.begin(),nums1.end());
+        unordered_set<int> uniqueElements;
 
-        int n = nums1.size(), m = nums2.size();
-        vector<int> resp;
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-
-        int i=0,j=0;
-        while(i<n && j < m){
-            if(nums1[i] == nums2[j]){
-                if(resp.empty() || resp.back() != nums1[i]) resp.push_back(nums1[i]);
-                i++;j++;
-            }
-            else if(nums1[i] > nums2[j]){
-                j++;
-            }else{
-                i++;
+        for(int ele : nums2){
+            if(st.count(ele)){
+               uniqueElements.insert(ele);
             }
         }
-        return resp;
+        return vector<int>(uniqueElements.begin(),uniqueElements.end());
     }
 };
