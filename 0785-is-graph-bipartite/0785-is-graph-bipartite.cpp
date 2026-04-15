049873949decdc5bc142cd333a6eacd24ac20325
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2026-04-15 23:55:35 +0100

    Time: 0 ms (100%), Space: 17.4 MB (60.39%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
 
-    bool bfs(int start, vector<vector<int>> &adj, vector<int> &colored){
-        queue<int> q;
-        q.push(start);
-        colored[start] = 1;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            for(int neighbor : adj[node]){
-                if(colored[neighbor] == 0){
-                    if(colored[node] == 1){
-                        colored[neighbor] = 2;
-                    }else{
-                        colored[neighbor] = 1;
-                    }
-                    q.push(neighbor);
-                }else if(colored[node] == colored[neighbor]){
-                    return false;
+    // bool bfs(int start, vector<vector<int>> &adj, vector<int> &colored){
+    //     queue<int> q;
+    //     q.push(start);
+    //     colored[start] = 1;
+    //     while(!q.empty()){
+    //         int node = q.front();
+    //         q.pop();
+    //         for(int neighbor : adj[node]){
+    //             if(colored[neighbor] == 0){
+    //                 if(colored[node] == 1){
+    //                     colored[neighbor] = 2;
+    //                 }else{
+    //                     colored[neighbor] = 1;
+    //                 }
+    //                 q.push(neighbor);
+    //             }else if(colored[node] == colored[neighbor]){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    bool dfs(int node, int color, vector<int> &colored, vector<vector<int>> &adj){
+        colored[node] = color;
+
+        for(int neighbor : adj[node]){
+            if(colored[neighbor] == 0){ // its not colored yet
+                if(color == 1){
+                    if(dfs(neighbor,2,colored,adj) == false) return false;
+                }else if(color == 2){
+                    if(dfs(neighbor,1,colored,adj) == false) return false;
                 }
+            }else if(colored[neighbor] == color){ // has the same color as its parent
+                return false;
             }
         }
         return true;
+
     }
+
 
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
@@ -30,7 +49,7 @@ public:
         
         for(int i=0;i<n;i++){
             if(colored[i] == 0){
-                if(bfs(i,graph,colored) == false){
+                if(dfs(i,1,colored,graph) == false){
                     return false;
                 }
             }
