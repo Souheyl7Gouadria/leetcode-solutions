2c0af6d3f8b9e2442f435b002229032c50f6f035
souheyl gouadria <118611908+Souheyl7Gouadria@users.noreply.github.com>
2026-05-18 15:09:00 +0100

    Time: 0 ms (100%), Space: 10 MB (11.75%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap(stones.begin(),stones.end());
+
+        while(maxHeap.size() > 1){
+            int x = maxHeap.top();
+            maxHeap.pop();
+            int y = maxHeap.top();
+            maxHeap.pop();
+            if( x == y) continue;
+            if(x > y) maxHeap.push(x-y);
+        }
+
+        return maxHeap.size() == 1 ? maxHeap.top() : 0;
+    }
+};
